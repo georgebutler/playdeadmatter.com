610bb343661360c159374eedfbe4a2c46072f4cc
George Butler <georgebutleriv@gmail.com>
2019-08-01 11:47:27 -0700

    CSS changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,54 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const {
-    CleanWebpackPlugin
-} = require('clean-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-    entry: [
-        'jquery',
-        './src/js/index.js'
-    ],
-    module: {
-        rules: [{
-            test: /\.scss$/,
-            use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: './static/[name].bundle.css',
-                    }
-                },
-                {
-                    loader: 'extract-loader'
-                },
-                {
-                    loader: 'css-loader?-url'
-                },
-                {
-                    loader: 'postcss-loader'
-                },
-                {
-                    loader: 'sass-loader'
-                }
-            ]
-        }]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: 'Dead Matter | Quantum Integrity Software Inc.',
-            template: './src/index.html'
-        })
-    ],
-    resolve: {
-        extensions: ['.js', '.scss', '.css']
-    },
-    devServer: {
-        contentBase: './dist'
-    },
-    output: {
-        filename: './js/[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  entry: ["jquery", "./src/js/index.js"],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "./static/[name].bundle.css"
+            }
+          },
+          {
+            loader: "extract-loader"
+          },
+          {
+            loader: "css-loader?-url"
+          },
+          {
+            loader: "postcss-loader"
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Dead Matter | Quantum Integrity Software Inc.",
+      template: "./src/index.html"
+    }),
+    new CopyWebpackPlugin([{ from: "./src/assets/img", to: "./static/img" }])
+  ],
+  resolve: {
+    extensions: [".js", ".scss", ".css"]
+  },
+  devServer: {
+    contentBase: "./dist"
+  },
+  output: {
+    filename: "./js/[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  }
 };
