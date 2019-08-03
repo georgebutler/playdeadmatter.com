cb66ba8953315f7ded5141d438742fb831391b28
George Butler <georgebutleriv@gmail.com>
2019-08-03 14:55:40 -0700

    robots.txt

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ImageminPlugin = require("imagemin-webpack-plugin").default;
+const RobotstxtPlugin = require("robotstxt-webpack-plugin");
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   mode: process.env.NODE_ENV === "production" ? "production" : "development",
@@ -40,12 +41,15 @@ module.exports = {
       title: "Dead Matter | Quantum Integrity Software Inc.",
       template: "./src/index.html"
     }),
-    new CopyWebpackPlugin([{ from: "./src/assets/img", to: "./static/img" }]),
+    new CopyWebpackPlugin([
+      { from: "src/assets/robots.txt", to: "./" },
+      { from: "./src/assets/img", to: "./static/img" }
+    ]),
     new ImageminPlugin({
       disable: process.env.NODE_ENV !== "production",
       test: /\.(jpe?g|png|gif|svg)$/i,
       pngquant: {
-        quality: "80-90"
+        quality: "90-100"
       }
     })
   ],
